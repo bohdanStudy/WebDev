2978b822bef01d0e6d4e9c90ca5f709c93d44e37
bohdan <bohdan.mychka@oa.edu.ua>
2026-03-16 13:23:39 +0200

    Lab JS

---
+++ lab6/lab6.js
@@ -0,0 +1,39 @@
+// завдання 1
+let a = 5;
+let b = 2;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+// завдання 2
+let firstName = "Іван";
+let lastName = "Іванов";
+console.log(`Привіт ${firstName} ${lastName}`);
+
+// завдання 3
+let age = 30;
+if (age >= 18) {
+  console.log("Доступ дозволено")
+}
+else {
+  console.log("Доступ заборонено")
+}
+
+// завдання 4
+for (let i = 1; i <= 10; i++)
+{
+  console.log(i)
+}
+
+// завдання 5
+function square(number) {
+  return number * number;
+}
+console.log(square(5));
+
+// завдання 6
+let fruits = ["лимон", "яблуко", "апельсин"];
+fruits.push("груша");
+console.log(fruits);
+
